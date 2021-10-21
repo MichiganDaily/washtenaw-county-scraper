1aa3dea9ecd6fd762f4f9cc6e9ea5cad6b4c0de8
erxclau <ericlau41802@gmail.com>
2021-10-20 20:07:54 -0400

    Finish scraping

--- scrape.py
+++ scrape.py
@@ -1,127 +1,119 @@
 import requests
 from bs4 import BeautifulSoup
 from json import dump
-from pprint import pprint
 
 
 def num(n: str):
     return float(n.replace(",", "").replace("%", ""))
 
 
-def get_precinct_report(url: str):
-    return
-
-
 def get_canvass_report(url: str):
-    req = requests.get(url)
-    html = req.text
+    html = requests.get(url).text
     soup = BeautifulSoup(html, "html.parser")
-    title = soup.find("font", attrs={"class", "h2"}).text
-    time = soup.find("font", attrs={"class": "h4"}).text[21:]
 
     head = soup.find("div", attrs={"class": "gheader"}).find_all(
         "td", attrs={"align": "left"})
 
-    registered_voters = num(head[1].text)
-    ballots_cast = num(head[3].text)
-    voter_turnout = num(head[5].text)
-    total_precincts = num(head[7].text)
-    full_count_precincts = num(head[9].text)
-    full_count_precincts_percent = num(head[11].text)
-    partial_count_precincts = num(head[15].text)
-
     table = soup.find_all("table")[1]
-    precincts = table.find("tr", attrs={"class": "bb"})
-    total = table.find("tr", attrs={"class": "bbeven"})
 
-    _, total_yes, total_no = total.find_all("td")
-    total_yes_num, _, total_yes_percent = total_yes.find("b").contents
-    total_yes_num = num(total_yes_num)
-    total_yes_percent = num(total_yes_percent)
+    precincts = table.find_all("tr")
+    precincts.pop()
+    precincts.pop(0)
 
-    total_no_num, _, total_no_percent = total_no.find("b").contents
-    total_no_num = num(total_no_num)
-    total_no_percent = num(total_no_percent)
+    data = list()
 
-    print(
-        registered_voters,
-        ballots_cast,
-        voter_turnout,
-        total_precincts,
-        full_count_precincts,
-        full_count_precincts_percent,
-        partial_count_precincts,
-        total_yes_num,
-        total_yes_percent,
-        total_no_num,
-        total_no_percent,
-        sep="\n"
-    )
+    for precinct in precincts:
+        datum = precinct.find_all("td")
+        data.append({
+            "Precinct": datum[0].text,
+            "Yes": num(datum[1].text.strip()),
+            "No": num(datum[2].text.strip())
+        })
 
-
-def get_summary(index: int):
-    return
-
-
-def get_precincts_counted(precincts: list):
-    return
+    return {
+        "meta": {
+            "title": soup.find("font", attrs={"class", "h2"}).text,
+            "time": soup.find("font", attrs={"class": "h4"}).text[21:],
+            "registered_voters": num(head[1].text),
+            "ballots_cast": num(head[3].text),
+            "voter_turnout": num(head[5].text),
+            "total_precincts": num(head[7].text),
+            "full_count_precincts": num(head[9].text),
+            "full_count_precincts_percent": num(head[11].text),
+            "partial_count_precincts": num(head[15].text)
+        },
+        "data": data
+    }
 
 
 def get_summary_row(row):
-    row.pop(0)
-    label = row[0]
-    call = "bold" in label["class"]
-    label: str = label.text.replace('\xa0', ' ').strip()
-    precinct = num(row[1].text)
-    absentee = num(row[2].text)
-    total = num(row[3].text)
-    percent = num(row[4].text)
+    label = row[1]
     return {
-        "label": label, 
-        "call": call, 
-        "in_precinct_votes": precinct, 
-        "absentee_votes": absentee, 
-        "total_votes": total, 
-        "percent_votes": percent
+        "label": label.text.replace('\xa0', ' ').strip(),
+        "call": "bold" in label["class"],
+        "in_precinct_votes": num(row[2].text),
+        "absentee_votes": num(row[3].text),
+        "total_votes": num(row[4].text),
+        "percent_votes": num(row[5].text)
     }
 
 
 def get_data(baseurl: str, indices: list):
-    req = requests.get(f"{baseurl}/index.jsp")
-    html = req.text
+    html = requests.get(f"{baseurl}/index.jsp").text
     soup = BeautifulSoup(html, "html.parser")
 
     tables = soup.find_all("table")
     summary = tables[2].find_all("tr")
 
+    precincts_counted = tables[3]
+    reported = dict()
+    precincts = precincts_counted.find_all("tr")
+    precincts.pop(0)
+    for precinct in precincts:
+        text = precinct.find("td").text.strip()
+        pclass = precinct.find("td")["class"]
+        if "red" in pclass:
+            reported[text] = "not-counted"
+        elif "blue" in pclass:
+            reported[text] = "partially-counted"
+        else:
+            reported[text] = "fully-counted"
+
     data = list()
 
     for index in indices:
-        details = summary[index + 1]
+        details = summary[index * 3 + 1]
         name = details.find(
             "td", attrs={"class": "headertr", "colspan": "3"}).text
-        canvass_url = details.find(
+        canvass = details.find(
             "td", attrs={"class": "headertr", "colspan": "2"}).find("a")["href"]
-        canvass_url = f"{baseurl}/{canvass_url}"
+        canvass = f"{baseurl}/{canvass}"
 
-        yes = get_summary_row(summary[index + 2].contents)
-        no = get_summary_row(summary[index + 3].contents)
+        yes = get_summary_row(summary[index * 3 + 2].contents)
+        no = get_summary_row(summary[index * 3 + 3].contents)
 
+        report = get_canvass_report(canvass)
+        for i in range(len(report["data"])):
+            report["data"][i]["counted"] = reported[report["data"][i]["Precinct"]]
 
         data.append({
             "name": name,
-            "canvass_url": canvass_url,
-            "options": [yes, no]
+            "options": [yes, no],
+            "report": report
         })
-        print(name, canvass_url, yes, no, sep="\n")
 
-    precincts_counted = tables[3]
-    return data
+    return {
+        "meta": {
+            "title": soup.find("font", attrs={"class", "h2"}).text,
+            "time": soup.find("font", attrs={"class": "h4"}).text[21:]
+        },
+        "data": data
+    }
 
 
 def main():
     url = "https://electionresults.ewashtenaw.org/electionreporting/aug2021"
-    data = get_data(url, [0])
+    data = get_data(url, [2, 3, 4, 5])
     with open("data.json", "w") as f:
         dump(data, f, indent=2)
 
